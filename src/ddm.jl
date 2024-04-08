35462eaf111af92f43630af3263b35391eba9357
dsryu0822 <rmsmsgood@naver.com>
2024-04-08 15:34:36 +0900

    fixed point 찾기 일부 성공

--- src/ddm.jl
+++ src/ddm.jl
@@ -56,16 +56,25 @@ include("../../setup/Dynamics.jl")
 
 const xmax = 0.5; const ymax = 0.5; resolution = 200
 qargs = (; xlims = [0, xmax], ylims = [0, ymax], xlabel = "young", ylabel = "old", size = [800, 800], framestyle = :box, formatter = x -> "$(round(100x; digits = 2))%")
-layer_ = [plot(; qargs...) for _ in 1:6]
 
 ### SINDy
 odata = dropmissing(select(data, Not(:ecnm)))
 sort(sort(odata, :Time), :ISO3)
 # data[data.ISO3 .== "KOR", :]
 
-f_ = [SINDy(odata[fold10 .!= k, :], [:dyng, :dold], [:yng, :old], N = setN) for setN in 1:20]
+
+
+f_ = [SINDy(odata, [:dyng, :dold], [:yng, :old], N = setN) for setN in 1:20]
 plot(log10.(maximum.(getproperty.(f_, :matrix))), xticks = 1:20, color = :black)
+plot(size.(getproperty.(f_, :matrix), 1), xticks = 1:20, color = :black)
 print(f_[20], ["y", "o"])
+try mkdir("G:/population/ddm/quiver") catch end
+for setN in 1:20
+    cf = zip(odata[:, 3:4].yng, odata[:, 3:4].old)
+    df = (f_[setN]).(eachrow(Matrix(odata[:, 3:4])))
+    quiver(first.(cf), last.(cf), quiver = (first.(df), last.(df)), size = [800, 800])
+    png("G:/population/ddm/quiver/N=$setN.png")
+end
 
 # setN = 6
 fold10 = rand(1:10, nrow(odata))
@@ -83,125 +92,52 @@ for setN = 1:20
 end
 plot(MSE_fold10, xticks = 1:20, color = :black)
 
-for setN in 1:20
-    f = SINDy(odata, [:dyng, :dold], [:yng, :old], N = setN)
-    cf = zip(odata[:, 3:4].yng, odata[:, 3:4].old)
-    df = f.(eachrow(Matrix(odata[:, 3:4])))
-    quiver(first.(cf), last.(cf), quiver = (first.(df), last.(df)), size = [800, 800])
-    png("N=$n")
+
+layer_ = [plot(; qargs...) for _ in 1:3]
+for setN = 11:20
+    mkdir("G:/population/ddm/N=$setN")
+    # f = SINDy(odata, [:dyng, :dold], [:yng, :old], N = setN)
+
+    ### Separatrix
+    traj_ = []
+    layer_[2] = deepcopy(layer_[1])
+    # pos_basin = grid(10, [0, .60], [0, .35])
+    pos_basin = grid(100, [0, .5], [0, .5])
+    @showprogress for ic = pos_basin
+        sol = solve(f_[setN], ic, tend = 1000, dense = true)
+        push!(traj_, ic => last(sol))
+        if !(0 < ic[1] < .5) || !(0 < ic[2] < .5) continue end
+        plot!(layer_[2], first.(sol), last.(sol), lw = 1, color = :black, alpha = 0.1)
+    end
+    zero2end = DataFrame(x0 = first.(first.(traj_)), y0 = last.(first.(traj_)), xend = first.(last.(traj_)), yend = last.(last.(traj_)))
+    CSV.write("G:/population/ddm/N=$(setN)/zero2end.csv", zero2end); # basin = CSV.read("basin.csv", DataFrame)
+
+    layer_[3] = deepcopy(layer_[1])
+    zero2end = zero2end[(0 .< zero2end.xend .< 0.5) .&& (0 .< zero2end.yend .< 0.5), :]
+    scatter!(layer_[3], zero2end.xend, zero2end.yend, color = :black, msw = 0)
+    # png(layer_[3], "layer_[3].png")
+
+    ### SINDy with external data
+    # edata = dropmissing(data)
+
+    # layer_[5] = deepcopy(layer_[1])
+    # color_ecnm = get(ColorSchemes.diverging_linear_bjr_30_55_c53_n256, edata.ecnm)
+    # for dat in eachrow(edata)
+    #     plot!(layer_[5], [dat.yng[1], dat.yng+dat.dyng[1]], [dat.old[1], dat.old+dat.dold[1]]
+    #     , arrow = arrow(:closed), color = get.(Ref(ColorSchemes.diverging_linear_bjr_30_55_c53_n256), dat.ecnm[1]), α = 0.5)
+    # end
+    # g = SINDy(edata, [:dyng, :dold], [:yng, :old, :ecnm], N = 6)
+    # print(g, ["y", "o", "e"])
+
+    # candy_ = []
+    # @showprogress for ee = 0:0.01:1.0
+    #     dir_e = g.(pos .⊕ [ee])
+    #     candy = pos[log10.(norm.(dir_e)) .< -4]
+    #     append!(candy_, candy .⊕ [ee])
+    # end
+    # scatter(first.(candy_), getindex.(candy_, 2), last.(candy_); qargs..., color = get(ColorSchemes.diverging_linear_bjr_30_55_c53_n256, last.(candy_)), xlims = [0.1, 0.5], ylims = [0.0, 0.2], zlabel = "economy", msw = 0)
+    # scatter(first.(candy_), getindex.(candy_, 2), color = get(ColorSchemes.diverging_linear_bjr_30_55_c53_n256, last.(candy_)); qargs..., xlims = [0.1, 0.5], ylims = [0.0, 0.2], msw = 0)
+
+    png.(layer_, ["G:/population/ddm/N=$(setN)/layer_[$(k)].png" for k in 1:length(layer_)])
+    rm("G:/population/ddm/N=$(setN)/layer_[1].png")
 end
-
-for setN = 1:20
-mkdir("G:/population/ddm/N=$setN")
-f = SINDy(odata, [:dyng, :dold], [:yng, :old], N = setN)
-
-# plot(0:20, MSE_, xticks = 0:20, ylims = [0, Inf], lw = 2, color = :black)
-Θxy = Θ([x, y], N = setN)
-∂xΘ = ∂x(Θxy)
-∂yΘ = ∂y(Θxy)
-
-### Find fixed points and Stability analysis
-layer_[2] = deepcopy(layer_[1])
-pos = grid(resolution, [0, xmax], [0, ymax])
-fixedpoint = eachfixed(f, pos)
-info_fp = DataFrame([1:length(fixedpoint) stack(fixedpoint, dims = 1)], ["Id", "x", "y"])
-info_fp.Id .= Int64.(info_fp.Id)
-info_fp[!, :norm] = norm.(f.(fixedpoint))
-
-eigenvalues = []
-for fp in fixedpoint
-    xy = Dict((x, y) .=> fp)
-    J = Symbolics.value.(substitute.([∂xΘ; ∂yΘ], Ref(xy)) * f.matrix)
-    push!(eigenvalues, eigen(J).values)
-end
-info_fp[!, :Re_λ1] = real.(first.(eigenvalues))
-info_fp[!, :Im_λ1] = imag.(first.(eigenvalues))
-info_fp[!, :Re_λ2] = real.(last.(eigenvalues))
-info_fp[!, :Im_λ2] = imag.(last.(eigenvalues))
-encode_stability = sum.(map(x -> x .> 0, real.(eigenvalues))) .+ 1
-info_fp[!, :stability] = [:stbl, :sddl, :unst][encode_stability]
-_mc = [:black, :lightgray, :white][encode_stability]
-CSV.write("data/info_fp_$setN.csv", info_fp, bom = true)
-scatter!(layer_[2], first.(fixedpoint), last.(fixedpoint), mc = _mc, ms = 10, msw = 2, txt = "       " .* ('A':'Z')[1:length(fixedpoint)])
-# end
-
-### Separatrix
-traj_ = []
-layer_[3] = deepcopy(layer_[1])
-# pos_basin = grid(10, [0, .60], [0, .35])
-pos_basin = grid(100, [0, .5], [0, .5])
-@showprogress for ic = pos_basin
-    sol = solve(f, ic, tend = 10000, dense = true)
-    push!(traj_, ic => last(sol))
-    if (ic[1] > .5) || (ic[2] > .4) continue end
-    plot!(layer_[3], first.(sol), last.(sol), lw = 1, color = :black, alpha = 0.1)
-end
-basin = DataFrame(x0 = first.(first.(traj_)), y0 = last.(first.(traj_)), xend = first.(last.(traj_)), yend = last.(last.(traj_)))
-CSV.write("G:/population/N=$(setN)/basin.csv", basin); # basin = CSV.read("basin.csv", DataFrame)
-# png(layer_[3], "layer_[3].png")
-
-### Basin of attraction
-# layer_[4] = deepcopy(layer_[1])
-# fixedpoint
-# temp = square((0.23 .< basin.xend .< 0.24) .&& (0.26 .< basin.yend .< 0.27) .&& (basin.y0 .< 0.65))'
-# # temp = square((0.115 .< basin.xend .< 0.116) .&& (0.228 .< basin.yend .< 0.229) .&& (basin.y0 .< 0.65))'
-# # temp = square((0.119 .< basin.xend .< 0.120) .&& (0.311 .< basin.yend .< 0.312) .&& (basin.y0 .< 0.65))'
-# scatter(first.(pos_basin[vec(temp)]), last.(pos_basin[vec(temp)]))
-# temp = (circshift(temp, [1,0]) + circshift(temp, [-1,0]) + circshift(temp, [0,1]) + circshift(temp, [0,-1]))
-# points = pos_basin[vec((temp .== 3)')]
-# # scatter(first.(points), last.(points))
-# points = points[sortperm([atan((pt - [.2, .2])...) for pt in points])]
-# push!(points, points[1])
-# plgn = DataFrame(stack(points, dims = 1), :auto) # CSV.write("plgn.csv", plgn);
-# plot!(layer_[4], first.(points), last.(points), lw = 2, color = :black)
-# plgn = CSV.read("plgn.csv", DataFrame)
-# plot!(layer_[4], plgn.x1, plgn.x2, lw = 2, color = :black)
-
-
-### SINDy with external data
-edata = dropmissing(data)
-
-layer_[5] = deepcopy(layer_[1])
-color_ecnm = get(ColorSchemes.diverging_linear_bjr_30_55_c53_n256, edata.ecnm)
-for dat in eachrow(edata)
-    plot!(layer_[5], [dat.yng[1], dat.yng+dat.dyng[1]], [dat.old[1], dat.old+dat.dold[1]]
-    , arrow = arrow(:closed), color = get.(Ref(ColorSchemes.diverging_linear_bjr_30_55_c53_n256), dat.ecnm[1]), α = 0.5)
-end
-g = SINDy(edata, [:dyng, :dold], [:yng, :old, :ecnm], N = 6)
-print(g, ["y", "o", "e"])
-
-candy_ = []
-@showprogress for ee = 0:0.01:1.0
-    dir_e = g.(pos .⊕ [ee])
-    candy = pos[log10.(norm.(dir_e)) .< -4]
-    append!(candy_, candy .⊕ [ee])
-end
-scatter(first.(candy_), getindex.(candy_, 2), last.(candy_); qargs..., color = get(ColorSchemes.diverging_linear_bjr_30_55_c53_n256, last.(candy_)), xlims = [0.1, 0.5], ylims = [0.0, 0.2], zlabel = "economy", msw = 0)
-scatter(first.(candy_), getindex.(candy_, 2), color = get(ColorSchemes.diverging_linear_bjr_30_55_c53_n256, last.(candy_)); qargs..., xlims = [0.1, 0.5], ylims = [0.0, 0.2], msw = 0)
-
-### Some trajectories
-layer_[6] = deepcopy(layer_[1]);
-function trajectory!(inputplot, ic, tend)
-    sol = solve(f, ic, tend = tend, dense = true)
-    plot!(inputplot, first.(sol), last.(sol), lw = 1, color = :black, arrow = true);
-end
-trajectory!(layer_[6], [.20, .55], 1.1)
-trajectory!(layer_[6], [.40, .50], 1.1)
-trajectory!(layer_[6], [.12, .34], 5)
-trajectory!(layer_[6], [.25, .34], 5)
-trajectory!(layer_[6], [.13, .34], 5)
-trajectory!(layer_[6], [.16, .34], 5)
-trajectory!(layer_[6], [.50, .20], 5)
-trajectory!(layer_[6], [.20, .11], 57)
-trajectory!(layer_[6], [.30, .10], 75)
-trajectory!(layer_[6], [.20, .10], 75)
-trajectory!(layer_[6], [.06, .10], 10)
-trajectory!(layer_[6], [.50, .10], 5)
-trajectory!(layer_[6], [.45, .08], 300)
-trajectory!(layer_[6], [.40, .05], 50)
-trajectory!(layer_[6], [.60, .03], 5)
-trajectory!(layer_[6], [.30, .03], 50)
-
-png.(layer_, ["G:/population/N=$(setN)/layer_[$(k)].png" for k in 1:6])
-end
-
--- src/leslie.jl
+++ src/leslie.jl
@@ -1,32 +1,49 @@
-λ = -0.01
-
-y0 = 0.198
-o0 = 0.221
-
-balance(λ) = o0*sum(exp.(λ*(1:15))) - y0*sum(exp.(λ*(65:100)))
-
-searchrange = -(0.01:0.000001:0.011)
-λ0 = searchrange[argmin(abs.(balance.(searchrange)))]
-
-pop0 = exp.(λ0*(1:100))
-
 using Plots
-
 include("../../setup/Dynamics.jl")
+
+mine(x, α, λ₁, λ₂) = α*exp(-λ₁*x) + (1-α)*λ₂*log(101 - x)
+
+plot(mine.(0:100, 0, 1, 1))
+plot(mine.(0:100, 0.5, 0.1, 0.5))
+plot(mine.(0:100, 1, 1, 1))
+
+balance(αλ) = o0*sum(mine.(1:15, αλ...)) - y0*sum(mine.(65:100, αλ...))
+
+y0, o0 = 0.198, 0.221
+αλ0 = findfixed(balance, [0.0, 0.1, 0.5], h = .0001)
+pop0 = mine.(1:100, αλ0...)
 plot(pop0, xticks = [0, 15, 65, 100], ylims = [0, Inf], color = :black, lw = 2)
 
+y0, o0 = 0.19185957,    0.005762051
+αλ0 = findfixed(balance, [1.0, 0.1, 0.5], h = .0001)
+pop0 = mine.(1:100, αλ0...)
+plot(pop0, xticks = [0, 15, 65, 100], ylims = [0, Inf], color = :black, lw = 2)
+
+y0, o0 = 0.12348538,	0.261549377
+αλ0 = findfixed(balance, [1., 1., 1.], h = .00001)
+pop0 = mine.(1:100, αλ0...)
+plot(pop0, xticks = [0, 15, 65, 100], ylims = [0, Inf], color = :black, lw = 2)
+
+y0, o0 = 0.30635788,	0.121840009
+αλ0 = findfixed(balance, [0.0, 0.1, 0.1], h = .0001)
+pop0 = mine.(1:100, αλ0...)
+plot(pop0, xticks = [0, 15, 65, 100], ylims = [0, Inf], color = :black, lw = 2)
+
+# ----
+
+mortality = (pop0 ./ circshift(pop0, 1))[2:end]
+plot(1 .- mortality, yscale = :log10, color = :red, lw = 2)
+
 fertiliry(p) = exp.(-(((1:100) .- p[1])/p[2]) .^ 2)/p[3]
-birth(p) = exp(λ0) - sum(pop0 .* fertiliry(p))
+birth(p) = first(pop0) - sum(pop0 .* fertiliry(p))
 
-birth([15, 1, 1.0])
-
-p0 = findfixed(birth, [30., 1, 10], h = .1)
+p0 = findfixed(birth, [30., 1, 10], h = .01)
 plot(fertiliry(p0), xticks = [0, 15, 20, 25, 30, 35, 40, 45, 65, 100], color = :green, lw = 2)
 
 Leslie = zeros(100, 100)
 Leslie[1,:] = fertiliry(p0)
 for i in 2:100
-    Leslie[i,i-1] = exp(λ0)
+    Leslie[i,i-1] = mortality[i-1]
 end
 using SparseArrays
 sparse(Leslie)
